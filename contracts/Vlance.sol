47e152ecdf3f98c8ca883f5713e92936df90dcd0
ali kim <alikim@gmail.com>
2023-06-24 20:49:29 -0400

    working test file, fixed up contract

--- contracts/Vlance.sol
+++ contracts/Vlance.sol
@@ -1,22 +1,54 @@
 
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.0;
-// import '@sismo-core/sismo-connect-solidity/contracts/libs/SismoLib.sol';
+import '@sismo-core/sismo-connect-solidity/contracts/libs/SismoLib.sol';
 
 
-contract Vlance {
+contract Vlance is SismoConnect{
     address payable public owner;
     event verifiedGithubContributor(address contributor);
-    // string public name;
 
-    constructor() payable {
+    bytes16 public constant APP_ID = 0xc8296b55ab40894b58d094e76248898b;
+    bytes16 public constant GROUP_ID = 0xfb20933ed4261d329255c10c64c53ff0;
+
+
+    constructor() SismoConnect(buildConfig(APP_ID, false)) payable {
+        // isImpersonationMode is set to true
         owner = payable(msg.sender);
-        // name=theName;
     }
 
-    function checkSismoGithub(string memory sismoResponse) public {
-        if (keccak256(abi.encodePacked(sismoResponse)) == keccak256(abi.encodePacked("true"))){
-            emit verifiedGithubContributor(msg.sender);
-        }
+    
+    function checkSismoGithub(bytes memory sismoResponse) public returns (uint256) {
+        
+
+
+        AuthRequest[] memory auths = new AuthRequest[](2);
+        auths[0] = buildAuth({authType: AuthType.VAULT});
+        auths[1] = buildAuth({authType: AuthType.GITHUB});
+
+
+
+        ClaimRequest[] memory claims = new ClaimRequest[](1);
+        claims[0] = buildClaim({
+            groupId: GROUP_ID,
+            isSelectableByUser: true,
+            isOptional: false
+        });
+
+
+        SismoConnectVerifiedResult memory result =  verify({
+            responseBytes: sismoResponse,
+            // we want the user to prove that he owns a Sismo Vault
+            auths: auths,
+            claims: claims
+
+            // signature: buildSignature({message: abi.encode(receiver)})
+    });
+
+        uint256 userId = SismoConnectHelper.getUserId(result, AuthType.VAULT);
+        //AuthType.GITHUB
+
+    
+        return userId;
     }
 }
